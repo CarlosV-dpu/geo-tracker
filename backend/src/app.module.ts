c05dddc0102d896d7335eed22f7f6fc603333431
Kay <carlosdvaldez02@gmail.com>
2026-07-28 20:35:07 -0500

    logro: implement location websocket gateway with socket.io

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { LocationModule } from './location/location.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, LocationModule],
   controllers: [AppController],
   providers: [AppService],
 })
